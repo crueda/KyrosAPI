da915553795511bb543c1830a058fe63ccd2daa4
Carlos Rueda Morales <carlos.rueda@deimos-space.com>
2017-02-02 10:06:25 +0100

    aa

--- app/routes/tracking.js
+++ app/routes/tracking.js
@@ -243,7 +243,6 @@ router.post('/trackings/', function(req, res)
   }
 });
 
-
 /* POST. Se obtiene trackings de un vehiculo */
 /**
 * @api {post} /trackings/vehicle/:id Request all tracking from vehicle
