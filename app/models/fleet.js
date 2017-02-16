fcf414660884cb54cd01d9bfbf5738b76760aff7
Carlos Rueda Morales <carlos.rueda@deimos-space.com>
2017-02-16 16:03:01 +0100

    harharhar

--- app/models/fleet.js
+++ app/models/fleet.js
@@ -43,189 +43,26 @@ mongoose.connect('mongodb://' + dbMongoHost + ':' + dbMongoPort + '/' + dbMongoN
 // Crear un objeto para ir almacenando todo lo necesario
 var fleetModel = {};
 
-// Obtener todos las fleets
-fleetModel.getFleets = function (startRow, endRow, sortBy, username, callback) {
-  // Obtener las flotas del usuario
+// Obtener las flotas del usuario
+fleetModel.getFleets = function (username, callback) {
   fleetModel.getFleetsFromUsername(username, function (error, monitor_fleet) {
-    pool.getConnection(function (err, connection) {
-      if (connection) {
-        var sqlCount = 'SELECT count(*) as nrows FROM FLEET where FLEET_ID IN (' + monitor_fleet + ')';
-        log.debug("Query: " + sqlCount);
-        connection.query(sqlCount, function (err, row) {
-          if (row) {
-            var consulta = 'SELECT FLEET_ID as id, DESCRIPTION_FLEET as description, CONSIGNOR_ID as companyId FROM FLEET where FLEET_ID IN(' + monitor_fleet + ')';
-
-            var totalRows = row[0].nrows;
-
-            var sql = '';
-            var orderBy = '';
-
-            if (sortBy == null) {
-              orderBy = 'id';
-            }
-            else {
-              vsortBy = sortBy.split(',');
-              for (var i = 0; i < vsortBy.length; i++) {
-                if (vsortBy[i].charAt(0) == '-') {
-                  var element = vsortBy[i].substring(1, vsortBy[i].length);
-                  if (element == 'id' || element == 'description' || element == 'weight' || element == 'longitude' || element == 'latitude' || element == 'height') {
-                    if (orderBy == '')
-                      orderBy = element + ' desc';
-                    else
-                      orderBy = orderBy + ',' + element + ' desc';
-                  }
-                } else {
-                  var element = vsortBy[i];
-                  if (element == 'id' || element == 'description' || element == 'weight' || element == 'longitude' || element == 'latitude' || element == 'height') {
-                    if (orderBy == '')
-                      orderBy = element;
-                    else
-                      orderBy = orderBy + ',' + element;
-                  }
-                }
-              }
-            }
-
-            if (orderBy == '') {
-              orderBy = 'id';
-            }
-
-            if (startRow == null || endRow == null) {
-              sql = consulta + " ORDER BY " + orderBy;
-            }
-            else {
-              sql = consulta + " ORDER BY " + orderBy + " LIMIT " + (endRow - startRow + 1) + " OFFSET " + startRow;
-            }
-
-            log.debug("Query: " + sql);
-            connection.query(sql, function (error, rows) {
-              connection.release();
-              if (error) {
-                callback(error, null);
-              }
-              else {
-                callback(null, rows, totalRows);
-              }
-            });
-          }
-          else {
-            connection.release();
-            callback(null, []);
-          }
+    mongoose.connection.db.collection('FLEET', function (err, collection) {
+        collection.find({"id": {$in: monitor_fleet}}, { _id: 0}).toArray(function (err, docs) {
+            callback(null, docs);
         });
-      }
-      else {
-        callback(null, null);
-      }
-    }); // pool mysql conection
+      });
   });
 }
 
 // Obtener un fleet por su id
 fleetModel.getFleet = function (id, callback) {
-  pool.getConnection(function (err, connection) {
-    if (connection) {
-      var sql = 'SELECT FLEET_ID as id, DESCRIPTION_FLEET as description, CONSIGNOR_ID as companyId FROM FLEET WHERE FLEET_ID = ' + connection.escape(id);
-      log.debug("Query: " + sql);
-      connection.query(sql, function (error, row) {
-        connection.release();
-        if (error) {
-          callback(error, null);
-        }
-        else {
-          callback(null, row);
-        }
-      });
-    }
-    else {
-      callback(null, null);
-    }
-  });
-
-}
-
-// Actualizar una flota
-fleetModel.updateFleet = function (fleetData, callback) {
-  pool.getConnection(function (err, connection) {
-    if (connection) {
-      var sql = 'UPDATE FLEET SET ' +
-        'DESCRIPTION_FLEET = ' + connection.escape(fleetData.name) + ',' +
-        'CONSIGNOR_ID = ' + connection.escape(fleetData.companyId) + ' ' +
-        'WHERE FLEET_ID = ' + fleetData.id;
-
-      log.debug("Query: " + sql);
-      connection.query(sql, function (error, result) {
-        connection.release();
-        if (error) {
-          callback(error, null);
-        }
-        else {
-          callback(null, { "message": "success" });
-        }
-      });
-    }
-    else {
-      callback(null, null);
-    }
+  mongoose.connection.db.collection('FLEET', function (err, collection) {
+    collection.find({ 'id': id }, { _id: 0}).toArray(function (err, docs) {
+        callback(null, docs);
+    });
   });
 }
 
-//añadir una nueva flota
-fleetModel.insertFleet = function (fleetData, callback) {
-  pool.getConnection(function (err, connection) {
-    if (connection) {
-      var sql = 'INSERT INTO FLEET SET ' +
-        'DESCRIPTION_FLEET = ' + connection.escape(fleetData.name) + ',' +
-        'CONSIGNOR_ID = ' + connection.escape(fleetData.companyId);
-
-      log.debug("Query: " + sql);
-      connection.query(sql, function (error, result) {
-        if (error) {
-          callback(null, null);
-        }
-        else {
-          callback(null, { "insertId": result.insertId });
-        }
-      });
-    }
-    else {
-      callback(null, null);
-    }
-  });
-}
-
-// Eliminar una flota
-fleetModel.deleteFleet = function (id, callback) {
-  pool.getConnection(function (err, connection) {
-    if (connection) {
-      var sqlExists = 'SELECT FLEET_ID as id, DESCRIPTION_FLEET as description, CONSIGNOR_ID as companyId FROM FLEET WHERE FLEET_ID = ' + connection.escape(id);
-      log.debug("Query: " + sqlExists);
-      connection.query(sqlExists, function (err, row) {
-        //si existe la id del fleet a eliminar
-        if (row) {
-          var sqlPoi = 'DELETE FROM FLEET WHERE FLEET_ID = ' + connection.escape(id);
-          log.debug("Query: " + sqlPoi);
-          connection.query(sqlPoi, function (error, result) {
-            if (error) {
-              callback(error, null);
-            }
-            else {
-              // se devuelven los datos del elemento eliminado
-              callback(null, row);
-            }
-          });
-        }
-        else {
-          connection.release();
-          callback(null, { "message": "notExist" });
-        }
-      });
-    }
-    else {
-      callback(null, null);
-    }
-  });
-}
 
 fleetModel.getFleetsFromUsername = function (username, callback) {
   mongoose.connection.db.collection('USER', function (err, collection) {
--- app/routes/fleet.js
+++ app/routes/fleet.js
@@ -152,10 +152,6 @@ var access_log = require('tracer').console({
  * @apiDescription List of fleets
  * @apiSampleRequest https://api.kyroslbs.com/fleets
  *
- * @apiParam {Number} [startRow] Number of first element
- * @apiParam {Number} [endRow] Number of last element
- * @apiParam {String="id","name",description","companyId"}  [sortBy]     Results sorting by this param. You may indicate various parameters separated by commas. To indicate descending order you can use the - sign before the parameter
- *
  * @apiSuccess {Object[]} fleet       List of fleets
  * @apiSuccessExample Success-Response:
  *     https/1.1 200 OK
@@ -163,24 +159,15 @@ var access_log = require('tracer').console({
  *       "response" :
  *       {
  *         "status" : 0,
- *         "startRow" : 0,
- *         "endRow" : 2,
- *         "totalRows" : 2,
- *         "data" : {
- *         "record" : [
+ *         "data" : [
  *           {
  *              "id": 123,
- *              "name": "Flota1",
- *              "description": "Flota general",
- *              "companyId": 34
+ *              "name": "Flota1"
  *           },
  *           {
  *              "id": 124,
- *              "name": "Flota2",
- *              "description": "Flota auxiliar",
- *              "companyId": 34
+ *              "name": "Flota2"
  *           }]
- *        }
  *       }
  *     }
  *
@@ -193,31 +180,16 @@ router.post('/fleets/', function(req, res)
 {
     log.info("POST: /fleets");
 
-    var startRow = req.body.startRow;
-    var endRow = req.body.endRow;
-    var sortBy = req.body.sortBy;
-    // Limpiar espacios en blanco
-    if (sortBy!=null) {
-      sortBy = sortBy.replace(/\s/g, "");
-    }
-
     var username = utils.getUsernameFromToken(req);
-    fleetModel.getFleets(startRow, endRow, sortBy, username, function(error, data, totalRows)
+    fleetModel.getFleets(username, function(error, data, totalRows)
     {
         if (data == null)
         {
-          res.status(200).json({"response": {"status":0,"data": {"record": []}}})
+          res.status(200).json({"response": {"status":0,"data": []}})
         }
         else if (typeof data !== 'undefined')
         {
-          if (startRow == null || endRow == null) {
-            startRow = 0;
-            endRow = totalRows;
-          }
-          else if (totalRows -1 <= endRow) {
-            endRow = totalRows - 1;
-          }
-          res.status(200).json({"response": {"status":0,"totalRows":totalRows,"startRow":parseInt(startRow),"endRow":parseInt(endRow),"status":0,"data": { "record": data}}})
+          res.status(200).json({"response": {"status":0, "data": data}})
         }
         else
         {
@@ -247,18 +219,11 @@ router.post('/fleets/', function(req, res)
  *       "response" :
  *       {
  *         "status" : 0,
- *         "startRow" : 0,
- *         "endRow" : 1,
- *         "totalRows" : 1,
- *         "data" : {
- *         "record" : [
+ *         "data" : [
  *           {
  *              "id": 123,
- *              "name": "Flota1",
- *              "description": "Flota general",
- *              "companyId": 34
+ *              "name": "Flota1"
  *           }]
- *        }
  *       }
  *     }
  *
@@ -290,7 +255,7 @@ router.get('/fleet/:id', function(req, res)
             //si existe se envia el json
             if (typeof data !== 'undefined' && data.length > 0)
             {
-                res.status(200).json({"response": {"status":0,"totalRows":1,"startRow":0,"endRow":1,"data": {"record": data}}})
+                res.status(200).json({"response": {"status":0, "data": data}});
             }
             //en otro caso se muestra un error
             else
@@ -307,258 +272,4 @@ router.get('/fleet/:id', function(req, res)
     }
 });
 
-/*
- * @api {put} /fleet/ Update fleet
- * @apiName PutNewFleet
- * @apiGroup Fleet
- * @apiVersion 1.0.1
- * @apiDescription Update fleet
- * @apiSampleRequest https://api.kyroslbs.com/fleet
- *
- * @apiParam {Number} id fleet unique ID
- * @apiParam {String} name Name of the fleet
- * @apiParam {String} description Description of the fleet
- * @apiParam {Number} companyId company ID
- *
- * @apiSuccess {json} message Result message
- * @apiSuccessExample Success-Response:
- *     https/1.1 200 OK
- *     {
- *       "response" :
- *       {
- *         "status" : 0,
- *         "data" : {
- *         "record" : [
- *           {
- *              "id": 123,
- *              "name": "Flota1",
- *              "description": "Flota general",
- *              "companyId": 34
- *           }]
- *        }
- *       }
- *     }
- *
- * @apiUse TokenHeader
- * @apiUse LoginError
- * @apiUse TokenError
- * @apiUse TokenExpiredError
- * @apiUse MissingParameterError
- */
-/*
-router.put('/fleet', function(req, res)
-{
-    log.info("PUT: /fleet");
-
-    var id_value = req.body.id 
-    var name_value = req.body.name 
-    var description_value = req.body.description 
-    var companyId_value = req.body.companyId 
-
-    log.debug("  -> id:          " + id_value);
-    log.debug("  -> name:        " + name_value);
-    log.debug("  -> description: " + description_value);
-    log.debug("  -> companyId:   " + companyId_value);
-
-    access_log.info("BODY >>> " + "id: " + id_value + " | name: "+ name_value + " | description: "+ description_value + " | companyId: " + companyId_value);
-
-    if (id_value == null || description_value == null || name_value == null || companyId_value == null) {
-      res.status(202).json({"response": {"status":status.STATUS_VALIDATION_ERROR,"description":messages.MISSING_PARAMETER}})
-    }
-    else
-    {
-      var fleetData = {
-          id : id_value,
-          name : name_value,
-          description : description_value,
-          companyId : companyId_value
-      };
-      fleetModel.updateFleet(fleetData,function(error, data)
-      {
-          if (data == null)
-          {
-            res.status(202).json({"response": {"status":status.STATUS_FAILURE,"description":messages.DB_ERROR}})
-          }
-          else
-          {
-            //si se ha actualizado correctamente mostramos un mensaje
-            if(data && data.message)
-            {
-              res.status(200).json({"response": {"status":0,"data": {"record": [fleetData]}}})
-            }
-            else
-            {
-              res.status(202).json({"response": {"status":status.STATUS_FAILURE,"description":messages.DB_ERROR}})
-            }
-          }
-      });
-    }
-});
-*/
-/*
- * @api {post} /fleet/ Create new fleet
- * @apiName PostNewFleet
- * @apiGroup Fleet
- * @apiVersion 1.0.1
- * @apiDescription Create new fleet
- * @apiSampleRequest https://api.kyroslbs.com/fleet
- *
- * @apiParam {String} name Name of the fleet
- * @apiParam {String} description Description of the fleet
- * @apiParam {Number} companyId company ID
- *
- * @apiSuccess {json} message Result message
- * @apiSuccessExample Success-Response:
- *     https/1.1 201 OK
- *     {
- *       "response" :
- *       {
- *         "status" : 0,
- *         "data" : {
- *         "record" : [
- *           {
- *              "id": 123,
- *              "name": "Flota1",
- *              "description": "Flota general",
- *              "companyId": 34
- *           }]
- *        }
- *       }
- *     }
- *
- * @apiUse TokenHeader
- * @apiUse LoginError
- * @apiUse TokenError
- * @apiUse TokenExpiredError
- * @apiUse MissingParameterError
- */
-/*
-router.post("/fleet", function(req,res)
-{
-    log.info("POST: /fleet");
-
-    // Crear un objeto con los datos a insertar del fleet
-    var name_value = req.body.name 
-    var description_value = req.body.description 
-    var companyId_value = req.body.companyId 
-
-    log.debug("  -> name:        " + name_value);
-    log.debug("  -> description: " + description_value);
-    log.debug("  -> companyId:   " + companyId_value);
-
-    access_log.info("BODY >>> " + "name: "+ name_value + " | description: "+ description_value + " | companyId: " + companyId_value);
-
-    if (description_value == null || name_value == null || companyId_value == null) {
-      res.status(202).json({"response": {"status":status.STATUS_VALIDATION_ERROR,"description":messages.MISSING_PARAMETER}})
-    }
-    else if (typeof parseInt(companyId_valua) != "number")
-    {
-      res.status(202).json({"response": {"status":status.STATUS_VALIDATION_ERROR,"description":messages.PARAMETER_ERROR_TYPE}})
-    }
-    else
-    {
-      var fleetData = {
-          id : null,
-          name : name_value,
-          description : description_value,
-          companyId : companyId_value
-      };
-
-      fleetModel.insertFleet(fleetData,function(error, data)
-      {
-        if (data == null)
-        {
-          res.status(202).json({"response": {"status":status.STATUS_FAILURE,"description":messages.DB_ERROR}})
-        }
-        else
-        {
-          // si se ha insertado correctamente mostramos su messaje de exito
-          if(data && data.insertId)
-          {
-              fleetData.id = data.insertId;
-              res.status(201).json({"response": {"status":0,"data": {"record": [fleetData]}}})
-          }
-          else
-          {
-             res.status(202).json({"response": {"status":status.STATUS_FAILURE,"description":messages.DB_ERROR}})
-          }
-        }
-      });
-    }
-});
-*/
-/*
- * @api {delete} /fleet Delete fleet
- * @apiName Deletefleet
- * @apiGroup Fleet
- * @apiVersion 1.0.1
- * @apiDescription Delete fleet
- * @apiSampleRequest https://api.kyroslbs.com/fleet
- *
- * @apiParam {Number} id fleet unique ID
- *
- * @apiSuccess {json} message Result message
- * @apiSuccessExample Success-Response:
- *     https/1.1 200 OK
- *     {
- *       "response" :
- *       {
- *         "status" : 0,
- *         "data" : {
- *         "record" : [
- *           {
- *              "id": 123,
- *              "name": "Flota1",
- *              "description": "Flota general",
- *              "companyId": 34
- *           }]
- *        }
- *       }
- *     }
- *
- * @apiUse TokenHeader
- * @apiUse LoginError
- * @apiUse TokenError
- * @apiUse TokenExpiredError
- * @apiUse MissingParameterError
- */
-/*
-router.delete("/fleet/:id", function(req, res)
-{
-    var id = req.params.id;
-    
-    log.info("DELETE: /fleet/"+id);
-    access_log.info("PARAMS >>> " + "id: " + id);
-
-    if (id == null)
-    {
-      res.status(202).json({"response": {"status":status.STATUS_VALIDATION_ERROR,"description":messages.MISSING_PARAMETER}})
-    }
-    else if (typeof parseInt(id) != "number")
-    {
-      res.status(202).json({"response": {"status":status.STATUS_VALIDATION_ERROR,"description":messages.PARAMETER_ERROR_TYPE}})
-    }
-    else
-    {
-      fleetModel.deletefleet(id,function(error, data)
-      {
-        if (data == null)
-        {
-          res.status(202).json({"response": {"status":status.STATUS_FAILURE,"description":messages.DB_ERROR}})
-        }
-        else
-        {
-            if(data && data.message != "notExist")
-            {
-              res.status(200).json({"response": {"status":0,"data": {"record": data}}})
-            }
-            else
-            {
-              res.status(202).json({"response": {"status":status.STATUS_FAILURE,"description":messages.DB_ERROR}})
-            }
-        }
-        });
-      }
-  });
-*/
 module.exports = router;
