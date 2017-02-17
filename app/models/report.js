4e5c1a1020d97554649647b1ff3c5cc60ef2d273
Carlos Rueda Morales <carlos.rueda@deimos-space.com>
2017-02-17 19:07:04 +0100

    hmmm

---
+++ app/models/report.js
@@ -0,0 +1,97 @@
+var server = require('mongodb').Server;
+var mongoose = require('mongoose');
+var crypt     = require('crypt3');
+var moment     = require('moment');
+
+var PropertiesReader = require('properties-reader');
+var properties = PropertiesReader('kyrosapi.properties');
+
+// Definición del log
+var fs = require('fs');
+var log = require('tracer').console({
+    transport : function(data) {
+        //console.log(data.output);
+        fs.open(properties.get('main.log.file'), 'a', 0666, function(e, id) {
+            fs.write(id, data.output+"\n", null, 'utf8', function() {
+                fs.close(id, function() {
+                });
+            });
+        });
+    }
+});
+
+var dbMongoName = properties.get('bbdd.mongo.name');
+var dbMongoHost = properties.get('bbdd.mongo.ip');
+var dbMongoPort = properties.get('bbdd.mongo.port');
+
+mongoose.connect('mongodb://' + dbMongoHost + ':' + dbMongoPort + '/' + dbMongoName, { server: { reconnectTries: 3, poolSize: 5 } }, function (error) {
+    if (error) {
+        log.info(error);
+    }
+});
+
+// Crear un objeto para ir almacenando todo lo necesario
+var reportModel = {};
+
+reportModel.getReportDailyData = function(vehicleLicense, callback)
+{
+    var start = moment().startOf('day');
+    mongoose.connection.db.collection('TRACKING_'+vehicleLicense, function (err, collection) {
+      collection.find({'pos_date': {$gt:Number(start)}}).sort({"pos_date":1}).toArray(function(err, docs) {
+        var out = {
+            "reportDailyStartDate" :  "",
+            "reportDailyStartGeocoding" :  "",
+            "reportDailyEndDate" :  "",
+            "reportDailyEndGeocoding" :  "",
+            "reportDailyDuration" :  0,
+            "reportDailyAverageSpeed" :  0,
+            "reportDailyMaxSpeed" :  0,
+            "reportDailyDistance" :  0,
+            "reportDailyConsumption" :  0,
+            "reportDailyCO2" :  0,
+            "reportDailyEventsStart" :  0,
+            "reportDailyEventsStop" :  0,
+            "reportDailyEventsUnplug" :  0,
+            "reportDailyEventsMaxSpeed" :  0,
+            "tracking" :  []
+            };
+          for (var i=0; i<docs.length;i++) {
+              var posDateInit = 0;
+              var posDateEnd = 0;
+              var sumSpeed = 0;
+              var count = 0;
+              if (i==0) {
+                  var epochStart = moment(new Date(docs[i].pos_date));
+                  out.reportDailyStartDate = epochStart.format("HH:mm:ss");
+                  out.reportDailyStartGeocoding = docs[i].geocoding;
+                  posDateInit = docs[i].pos_date;
+              } else if (i==docs.length) {
+                  var epochEnd = moment(new Date(docs[i].pos_date));
+                  out.reportDailyEndtDate = epochEnd.format("HH:mm:ss");
+                  out.reportDailyEndGeocoding = docs[i].geocoding;
+                  posDateEnd = docs[i].pos_date;
+              }
+              out.reportDailyDistance = out.reportDailyDistance + docs[i].distance;
+              if (docs[i].speed > out.reportDailyMaxSpeed) {
+                  out.reportDailyMaxSpeed = docs[i].speed.toFixed(2);
+              }
+              sumSpeed = sumSpeed + docs[i].speed;
+              count = count + 1;
+
+          }
+          var duration = posDateEnd - posDateInit;
+          var dateDuration = new Date(posDateEnd - posDateInit);
+          var h = dateDuration.getHours();
+          var m = dateDuration.getMinutes();
+          var s = dateDuration.getSeconds();
+          out.reportDailyDuration = h + ":" + m + ":" + s;
+
+          out.reportDailyAverageSpeed = (sumSpeed / count).toFixed(2);
+
+          callback(null, out);
+      });
+  });
+}
+
+//exportamos el objeto para tenerlo disponible en la zona de rutas
+module.exports = reportModel;
---
+++ app/routes/app_report.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+
+var status = require("../utils/statusCodes.js");
+var messages = require("../utils/statusMessages.js");
+
+// Fichero de propiedades
+var PropertiesReader = require('properties-reader');
+var properties = PropertiesReader('kyrosapi.properties');
+
+var ReportModel = require('../models/report');
+
+// Definición del log
+var fs = require('fs');
+var log = require('tracer').console({
+  transport : function(data) {
+    //console.log(data.output);
+    fs.open(properties.get('main.log.file'), 'a', 0666, function(e, id) {
+      fs.write(id, data.output+"\n", null, 'utf8', function() {
+        fs.close(id, function() {
+        });
+      });
+    });
+  }
+});
+
+router.post('/app/report_daily/vehicle/:vehicleLicense', function(req, res)
+{
+    var vehicleLicense = req.params.vehicleLicense;
+    log.info("POST: /app/report_daily/vehicle/"+vehicleLicense);
+
+    if (vehicleLicense==null) {
+      res.status(202).json({"response": {"status":status.STATUS_VALIDATION_ERROR,"description":messages.MISSING_PARAMETER}})
+    }
+    else {
+      ReportModel.getReportDailyData(vehicleLicense,function(error, data)
+      {
+        if (data == null)
+        {
+          res.status(202).json({"response": {"status":status.STATUS_FAILURE,"description":messages.DB_ERROR}})
+        }
+        else
+        {
+          //si existe enviamos el json
+          if (typeof data !== 'undefined')
+          {
+            res.status(200).json(data)
+          }
+          else
+          {
+            res.status(202).json({"response": {"status":status.STATUS_NOT_FOUND_REGISTER,"description":messages.MISSING_REGISTER}})
+          }
+        }
+      });
+    }
+});
+
+module.exports = router;
