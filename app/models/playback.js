31689efe92b0c24ee3c4f90af5241673e1f7bf32
Carlos Rueda Morales <crueda@gmail.com>
2017-10-24 11:10:19 +0200

    TDD: 1, Me: 0

--- app/models/playback.js
+++ app/models/playback.js
@@ -32,16 +32,83 @@ mongoose.connect('mongodb://' + dbMongoHost + ':' + dbMongoPort + '/' + dbMongoN
 var playbackModel = {};
 
 playbackModel.getKmlFromToken = function (token, callback) {
+  function padToTwo(number) {
+    if (number <= 9) { number = ("0" + number).slice(-4); }
+    return number;
+  }
   mongoose.connection.db.collection('PLAYBACK', function (err, collection) {
     collection.find({ "token": token }).toArray(function (err, docs) { 
       if (docs!=undefined && docs[0]!=undefined) {
         mongoose.connection.db.collection('TRACKING', function (err, collection) {
-          //var initEpoch = moment(initDate, 'YYYY-MM-DDTHH:mm:ssZ');
-          //var endEpoch = moment(endDate, 'YYYY-MM-DDTHH:mm:ssZ');
             collection.find({ 'device_id': parseInt(docs[0].device_id), 'pos_date': { $gt: parseInt(docs[0].init_date), $lt: parseInt(docs[0].end_date) } }).sort({ 'pos_date': 1 }).limit(7000).toArray(function (err, docsTracking) {
 
-            var kml_init_date =  moment(docsTracking[0].pos_date).format("YYYY-MM-DDTHH:mm:ss.SSS");
+            if (docsTracking!=undefined && docsTracking.length>1) {
+              var kml_init_date =  moment(docsTracking[0].pos_date).format("YYYY-MM-DDTHH:mm:ss.SSS");
+
+              var dateDuration = new Date(docsTracking[docsTracking.length-1].pos_date - docsTracking[0].pos_date);
+              var dateDurationUTC = new Date(dateDuration.getUTCFullYear(), dateDuration.getUTCMonth(), dateDuration.getUTCDate(), dateDuration.getUTCHours(), dateDuration.getUTCMinutes(), dateDuration.getUTCSeconds());
+              var h = dateDurationUTC.getHours();
+              var m = dateDurationUTC.getMinutes();
+              var s = dateDurationUTC.getSeconds();
+              totalIsoDate = padToTwo(h) + ":" + padToTwo(m) + ":" + padToTwo(s);
+
+              var initIsoDate = "";
+              var endIsoDate = "";
+              if (docs[0].time_zone!=undefined) {
+                initIsoDate = moment.tz(new Date(docsTracking[0].pos_date), docs[0].time_zone).format("DD/MM/YYYY HH:mm:ss");
+                endIsoDate = moment.tz(new Date(docsTracking[docsTracking.length-1].pos_date), docs[0].time_zone).format("DD/MM/YYYY HH:mm:ss");                
+              } else {
+                initIsoDate = moment.tz(new Date(docsTracking[0].pos_date), "Europe/Madrid").format("DD/MM/YYYY HH:mm:ss");
+                endIsoDate = moment.tz(new Date(docsTracking[docsTracking.length-1].pos_date), "Europe/Madrid").format("DD/MM/YYYY HH:mm:ss");                
+              }
               
+              var lon_init = docsTracking[0].location.coordinates[0];
+              var lat_init = docsTracking[0].location.coordinates[1];
+              var altitude_init = docsTracking[0].altitude;
+
+              var lon_end = docsTracking[docsTracking.length-1].location.coordinates[0];
+              var lat_end = docsTracking[docsTracking.length-1].location.coordinates[1];
+              var altitude_end = docsTracking[docsTracking.length-1].altitude;
+
+              var distance = 0;
+              var min_speed = 9999;
+              var max_speed = 0;
+              var sum_speed = 0;
+              var min_altitude = 999999;
+              var max_altitude = 0;
+              var sum_altitude = 0;
+
+              var kml_data = "";
+              for (var i=0; i<docsTracking.length;i++) {
+                if (i>0) {
+                  distance += docsTracking[i].distance;
+                }
+                if (docsTracking[i].speed > max_speed) {
+                  max_speed = docsTracking[i].speed;
+                }
+                if (docsTracking[i].speed < min_speed) {
+                  min_speed = docsTracking[i].speed;
+                }
+                sum_speed += docsTracking[i].speed;
+                if (docsTracking[i].altitude > max_altitude) {
+                  max_altitude = docsTracking[i].altitude;
+                }
+                if (docsTracking[i].altitude < min_altitude) {
+                  min_altitude = docsTracking[i].altitude;
+                }
+                sum_altitude += docsTracking[i].altitude;
+
+                var kml_date =  moment(docsTracking[i].pos_date).format("YYYY-MM-DDTHH:mm:ss.SSS");
+                var lon = docsTracking[i].location.coordinates[0];
+                var lat = docsTracking[i].location.coordinates[1];
+                var altitude = docsTracking[i].altitude;
+                kml_data = kml_data + `
+                <when>`;
+                kml_data = kml_data + kml_date + `Z</when>`;
+                kml_data = kml_data + `
+                <gx:coord>` + lon + ` ` + lat + ` ` + altitude + `</gx:coord>`;
+              }              
+
 var kml_init = `<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2"
 xmlns:gx="http://www.google.com/kml/ext/2.2"
@@ -78,35 +145,29 @@ xmlns:atom="http://www.w3.org/2005/Atom">
 <hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>
 </IconStyle></Style>
 <Schema id="schema">
-<gx:SimpleArrayField name="power" type="int">
-<displayName><![CDATA[Power (W)]]></displayName>
-</gx:SimpleArrayField>
-<gx:SimpleArrayField name="cadence" type="int">
-<displayName><![CDATA[Cadence (rpm)]]></displayName>
-</gx:SimpleArrayField>
-<gx:SimpleArrayField name="heart_rate" type="int">
-<displayName><![CDATA[Heart rate (bpm)]]></displayName>
-</gx:SimpleArrayField>
 </Schema>
 <Placemark>
-<name><![CDATA[Schuylkill River Trail Bike Ride (Start)]]></name>
+<name>Inicio</name>
 <description><![CDATA[]]></description>
 <TimeStamp><when>` + kml_init_date + `Z</when></TimeStamp>
 <styleUrl>#start</styleUrl>
 <Point>
-<coordinates>-75.300238,40.109584,-2.799999952316284</coordinates>
+<coordinates>` + lon_init + ` ` + lat_init + ` ` + altitude_init + `</coordinates>
 </Point>
 </Placemark>
 <Placemark id="tour">
 <name><![CDATA[` + docs[0].vehicle_license + `]]></name>
 <description><![CDATA[
-Name: Playback<br>
-Total distance: 26.30 km (16.3 mi)<br>
-Total time: 1:33:41<br>
-Moving time: 1:32:26<br>
-Average speed: 16.84 km/h (10.5 mi/h)<br>
-Average moving speed: 17.07 km/h (10.6 mi/h)<br>
-Max speed: 36.90 km/h (22.9 mi/h)<br>
+Fecha inicial:    ` + initIsoDate + `<br>
+Fecha final:      ` + endIsoDate + `<br>
+Tiempo:           ` + totalIsoDate + `<br>
+Distancia:        ` + distance.toFixed(3) + ` Km<br>
+Velocidad mínima: ` + min_speed.toFixed(2) + ` Km/h<br>
+Velocidad máxima: ` + max_speed.toFixed(2) + ` Km/h<br>
+Velocidad media:  ` + (distance/(dateDuration/3600000)).toFixed(2) + ` Km/h<br>
+Altitud mínima:   ` + min_altitude.toFixed(0) + ` m<br>
+Altitud máxima:   ` + max_altitude.toFixed(0) + ` m<br>
+Altitud media:    ` + (sum_altitude/docsTracking.length).toFixed(0) + ` m<br>
 ]]></description>
 <styleUrl>#track</styleUrl>
 <ExtendedData>
@@ -117,34 +178,6 @@ Max speed: 36.90 km/h (22.9 mi/h)<br>
 <gx:interpolate>1</gx:interpolate>
 <gx:Track>`;
 
-var kml_data = "";
-for (var i=0; i<docsTracking.length;i++) {
-  var kml_date =  moment(docsTracking[i].pos_date).format("YYYY-MM-DDTHH:mm:ss.SSS");
-  var lon = docsTracking[i].location.coordinates[0];
-  var lat = docsTracking[i].location.coordinates[1];
-  kml_data = kml_data + `
-  <when>`;
-  kml_data = kml_data + kml_date + `Z</when>`;
-  kml_data = kml_data + `
-  <gx:coord>` + lon + ` ` + lat + ` 0` + `</gx:coord>`;
-}              
-
-var kml_old = `
-<when>2013-07-25T19:33:59.000Z</when>
-<gx:coord>-75.300234 40.109597 38.0</gx:coord>
-<when>2013-07-25T19:34:00.000Z</when>
-<gx:coord>-75.300214 40.109641 36.099998474121094</gx:coord>
-<when>2013-07-25T19:34:09.000Z</when>
-<gx:coord>-75.300233 40.109603 20.700000762939453</gx:coord>
-<when>2013-07-25T19:34:10.000Z</when>
-<gx:coord>-75.30025 40.109586 25.600000381469727</gx:coord>
-<when>2013-07-25T19:34:42.000Z</when>
-<gx:coord>-75.30026 40.10963 36.5</gx:coord>
-<when>2013-07-25T19:34:43.000Z</when>
-<gx:coord>-75.300061 40.10971 33.400001525878906</gx:coord>
-<when>2013-07-25T21:07:25.000Z</when>
-<gx:coord>-75.30001 40.109709 33.29999923706055</gx:coord>`;
-
 var kml_end = `
 <ExtendedData>
 <SchemaData schemaUrl="#schema">
@@ -154,17 +187,20 @@ var kml_end = `
 </gx:MultiTrack>
 </Placemark>
 <Placemark>
-<name><![CDATA[Schuylkill River Trail Bike Ride (End)]]></name>
+<name>Fin</name>
 <TimeStamp><when>` + kml_init_date + `Z</when></TimeStamp>
 <styleUrl>#end</styleUrl>
 <Point>
-<coordinates>-75.30001,40.109709,33.29999923706055</coordinates>
+<coordinates>` + lon_end + ` ` + lat_end + ` ` + altitude_end + `</coordinates>
 </Point>
 </Placemark>
 </Document>           
 </kml>`;
             
               callback(null, kml_init + kml_data + kml_end);
+            } else {
+              callback(null, ""); 
+            }
           });
         });
        } else {
